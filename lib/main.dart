8718320cb33de1c8a79cf424f79b41d825df53df
Alan Lopez <alandlopezg@outlook.com>
2026-08-08 11:25:17 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/config/theme/app_theme.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TokTik',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
