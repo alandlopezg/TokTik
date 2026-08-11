349107fc9a4d3465290d6b77e27b9ea4f4195bac
Alan Lopez <alandlopezg@outlook.com>
2026-08-11 18:50:45 -0300

    Third commit from Provider lesson

---
+++ lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/config/domain/entities/videos_post.dart';
+
+class DiscoverProvider extends ChangeNotifier {
+
+  List<VideoPost> videos = [];
+
+}
