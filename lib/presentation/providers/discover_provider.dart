32da0b8fa53952b93b5c9d3244d7d888bb807448
Alan Lopez <alandlopezg@outlook.com>
2026-08-16 14:50:03 -0300

    Fifth commit from Read and Assign videos from Provider Lesson

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/config/domain/entities/videos_post.dart';
+import 'package:toktik/infraestructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
 
+  //TODO: Repository, DataSource
+
   List<VideoPost> videos = [];
+  bool initialLoading = true;
+
+
+  Future<void> loadNextPage() async {
+
+    await Future.delayed(const Duration( seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts.map( 
+      ( video ) => LocalVideoModel.fromJson(video).toVideoPostEntity()
+      ).toList();
+
+      videos.addAll( newVideos );
+      initialLoading = false;
+      notifyListeners();
+    }
 
 }
